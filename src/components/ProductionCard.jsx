65970b2a1ad5b67c76933fa8ab68224de820bd54
Michael Bennett <mvbennett9@gmail.com>
2022-12-21 21:33:47 -0500

    foundation of all components set up

---
+++ src/components/ProductionCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProductionCard = () => {
+  return(
+    <div></div>
+  );
+};
+
+export default ProductionCard;
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,3 @@
 import '../assets/stylesheets/application.scss';
+import React from 'react';
+import { ReactDOM } from 'react';
