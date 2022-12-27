8551c8e2cab820e126599d5abade58d119c7d339
Michael Bennett <mvbennett9@gmail.com>
2022-12-26 22:03:27 -0500

    Set up the production card

--- src/components/ProductionCard.jsx
+++ src/components/ProductionCard.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
-const ProductionCard = () => {
+const ProductionCard = ({img, title, link, icons}) => {
   return(
-    <div></div>
+    <div>
+      <img src={img} alt={title} className="album-cover" />
+      <a href={link}>
+        {title}
+      </a>
+      <div className="production-icons">
+      {icons.forEach(icon => {
+        <img src={icon.img} alt={icon.name} />
+      })}
+      </div>
+    </div>
   );
 };
 
