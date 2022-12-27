29b743bc266cd8e2daa31a5e871bb11970e9f61d
Michael Bennett <mvbennett9@gmail.com>
2022-12-27 17:41:14 -0500

    made components sass directory

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Latest = () => {
   return (
     // latest project will go here
-    <div></div>
+    <div>
+      <img src="" alt="Underwater Supermarkets on Vinyl" />
+    </div>
   );
 };
 
