c20a94ac07f98dcb7ad91703545b47c76829a78b
Michael Bennett <mvbennett9@gmail.com>
2022-12-29 16:56:05 -0500

    set up projects and productions components

---
+++ src/components/Productions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductionCard from "./ProductionCard";
+
+const Productions = () => {
+  return (
+    <div>
+      <ProductionCard />
+    </div>
+  );
+};
+
+export default Productions;
