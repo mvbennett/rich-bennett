9b697b9f9233f27fbeb2eead972fbee567a4ec3c
Michael Bennett <mvbennett9@gmail.com>
2022-12-29 17:03:02 -0500

    router and history set up on index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,22 @@
 import '../assets/stylesheets/application.scss';
 import React from 'react';
 import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router, Route, Redirect, Switch
+} from 'react-router-dom';
+import { createHistory as history } from 'history';
 import App from './components/App';
 
 const container = document.getElementById('root');
 if (container) {
   const root = ReactDOM.createRoot(container);
-  root.render(<App />);
+  // root.render(<App />);
+  root.render(
+    <Router history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+        <Redirect from="/" to="/" />
+      </Switch>
+    </Router>
+  );
 }
