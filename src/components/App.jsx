e8be26a3a2ad8a926c6dc9db0538df78e345b7c8
Michael Bennett <mvbennett9@gmail.com>
2022-12-29 16:52:50 -0500

    created components for all projects and all productions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   return (
     <div>
       <Navbar page="home" />
+      {/* using router, set up a switch that will mount latest on default,
+      and the three pages otherwise */}
       <Latest />
     </div>
   );
