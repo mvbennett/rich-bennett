5aeca833a66dabcd77ed60fbd5a303a498f2f774
Michael Bennett <mvbennett9@gmail.com>
2023-01-02 15:09:06 -0500

    improved responsiveness for production icons and home page

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -8,11 +8,11 @@ const Bio = () => {
       <div className="container">
         <div className="bio-card">
           <div className="bio-pic">
-            <img src="" alt="Rich Bennett" />
+            <img src="https://images.squarespace-cdn.com/content/v1/5c3a67302714e564a6a8fc11/1622777719896-6KGUMG1N27WDCU7GKD0K/rich.jpg" alt="Rich Bennett" />
           </div>
           <div className="bio-info">
             <h3>
-              Hi, Im Rich Bennett
+              Hi, I am Rich Bennett
             </h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
